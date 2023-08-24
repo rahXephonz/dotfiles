b712f3e75a3cf1dea86cea072919da88b14dc1aa
Ioofy <mynev.id@gmail.com>
2023-08-24 18:55:29 +0700

    chore: update git config

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,8 +1,8 @@
 {
   programs.git = {
     enable = true;
-    userName = "Maulana Sodiqin";
-    userEmail = "maulanasdqn@gmail.com";
+    userName = "Rahxephonz";
+    userEmail = "mynev.id@gmail.com";
     extraConfig = {
       init = { defaultBranch = "main"; };
       push = { autoSetupRemote = true; };
